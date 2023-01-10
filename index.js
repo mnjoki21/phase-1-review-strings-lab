b8404cdd186ecf3c02ba50331fcfc92d32c2b782
mnjoki21 <mitchellenjoki7@gmail.com>
2023-01-11 00:00:11 +0300

    commit tree c27a2c0c8685aa1c6c0033c48fd4c6d2728f85b4

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 // Write your code in this file!
-const currentUser = 'mitchelle'
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = `Welcome, ${currentUser[0]}!`
+const currentUser = "Mitchelle"
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
